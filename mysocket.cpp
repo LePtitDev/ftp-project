7d46e70b14e833c62e922971c74f74b364b5de5d
Arthur Ferré <Arthur Ferr>
2016-11-23 10:19:04 +0100

    Ajout de class de socket basique pour faciliter leurs manipulations

---
+++ mysocket.cpp
@@ -0,0 +1,183 @@
+#include "mysocket.h"
+
+/****** CLASS ADDRESS ******/
+
+MySocket::Address::Address() {
+    this->s_addr.sin_family = AF_INET;
+    struct hostent * ht = gethostbyname("localhost");
+    if (ht) {
+        this->s_addr.sin_addr.s_addr = MySocket::Address::_getipbyname(ht);
+        MySocket::Address::_addrtoip(this->s_ip, &(this->s_addr));
+    }
+    else {
+        this->s_success = false;
+        return;
+    }
+    this->s_port = 0;
+    this->s_addr.sin_port = htons((short)this->s_port);
+    this->s_success = true;
+}
+
+MySocket::Address::Address(int portn) {
+    this->s_addr.sin_family = AF_INET;
+    struct hostent * ht = gethostbyname("localhost");
+    if (ht) {
+        this->s_addr.sin_addr.s_addr = MySocket::Address::_getipbyname(ht);
+        MySocket::Address::_addrtoip(this->s_ip, &(this->s_addr));
+    }
+    else {
+        this->s_success = false;
+        return;
+    }
+    this->s_port = portn;
+    this->s_addr.sin_port = htons((short)this->s_port);
+    this->s_success = true;
+}
+
+MySocket::Address::Address(const char * addr, int portn) {
+    this->s_addr.sin_family = AF_INET;
+    struct hostent * ht = gethostbyname(addr);
+    if (ht) {
+        this->s_addr.sin_addr.s_addr = MySocket::Address::_getipbyname(ht);
+        MySocket::Address::_addrtoip(this->s_ip, &(this->s_addr));
+    }
+    else {
+        this->s_success = false;
+        return;
+    }
+    this->s_port = portn;
+    this->s_addr.sin_port = htons((short)this->s_port);
+    this->s_success = true;
+}
+
+MySocket::Address::Address(const struct sockaddr_in * addr) {
+    this->s_addr.sin_family = addr->sin_family;
+    this->s_addr.sin_addr = addr->sin_addr;
+    this->s_addr.sin_port = addr->sin_port;
+    MySocket::Address::_addrtoip(this->s_ip, &(this->s_addr));
+    this->s_port = MySocket::Address::_addrtoportn(addr);
+    this->s_success = true;
+}
+
+bool MySocket::Address::Success() const {
+    return this->s_success;
+}
+
+const char* MySocket::Address::GetIP() const {
+    return this->s_ip;
+}
+
+int MySocket::Address::GetPort() const {
+    return this->s_port;
+}
+
+const struct sockaddr_in* MySocket::Address::GetSockaddr() const {
+    return &(this->s_addr);
+}
+
+void MySocket::Address::Refresh() {
+    MySocket::Address::_addrtoip(this->s_ip, &(this->s_addr));
+    this->s_port = MySocket::Address::_addrtoportn(&(this->s_addr));
+}
+
+in_addr_t MySocket::Address::_getipbyname(struct hostent * ht) {
+    return ((struct in_addr *)(ht->h_addr_list[0]))->s_addr;
+}
+
+void MySocket::Address::_addrtoip(char * dest, const struct sockaddr_in * addr) {
+    strcpy(dest, inet_ntoa(addr->sin_addr));
+}
+
+int MySocket::Address::_addrtoportn(const struct sockaddr_in * addr) {
+    return ntohs(addr->sin_port);
+}
+
+/****** CLASS SOCKET_TCP ******/
+
+MySocket::Socket_TCP::Socket_TCP() : c_addr() {
+    this->_init_serv();
+}
+
+MySocket::Socket_TCP::Socket_TCP(int portn) : c_addr(portn) {
+    this->_init_serv();
+}
+
+MySocket::Socket_TCP::Socket_TCP(const char * addr, int portn) : c_addr(addr, portn) {
+    if (!this->c_addr.Success()) {
+        this->s_success = false;
+        return;
+    }
+
+    this->s_socket = socket(PF_INET, SOCK_STREAM, 0);
+    if (this->s_socket == -1) {
+        this->s_success = false;
+        return;
+    }
+
+    int res = connect(this->s_socket, (const struct sockaddr *) this->c_addr.GetSockaddr(), sizeof(struct sockaddr_in));
+    if (res < 0) {
+        this->s_success = false;
+        close(this->s_socket);
+        return;
+    }
+
+    this->c_addr.Refresh();
+
+    this->s_success = true;
+}
+
+bool MySocket::Socket_TCP::Success() {
+    return this->s_success;
+}
+
+bool MySocket::Socket_TCP::Listen(int size) {
+    return (listen(this->s_socket, size) < 0);
+}
+
+MySocket::Socket_TCP MySocket::Socket_TCP::Accept() {
+    struct sockaddr_in addr;
+    socklen_t len;
+    int descr = accept(this->s_socket, (struct sockaddr *)&addr, &len);
+    return MySocket::Socket_TCP(descr, &addr);
+}
+
+const MySocket::Address& MySocket::Socket_TCP::GetDestination() {
+    return this->c_addr;
+}
+
+int MySocket::Socket_TCP::Read(void * buf, int max) {
+    return recv(this->s_socket, buf, max, 0);
+}
+
+int MySocket::Socket_TCP::Write(void * datas, int len) {
+    return send(this->s_socket, datas, len, 0);
+}
+
+MySocket::Socket_TCP::Socket_TCP(int descr, struct sockaddr_in * addr) : c_addr(addr) {
+    this->s_socket = descr;
+    this->s_success = true;
+}
+
+void MySocket::Socket_TCP::_init_serv() {
+    if (!this->c_addr.Success()) {
+        this->s_success = false;
+        return;
+    }
+
+    this->s_socket = socket(PF_INET, SOCK_STREAM, 0);
+    if (this->s_socket == -1) {
+        this->s_success = false;
+        return;
+    }
+
+    int res = bind(this->s_socket, (const struct sockaddr *) this->c_addr.GetSockaddr(), sizeof(struct sockaddr_in));
+    if (res < 0) {
+        this->s_success = false;
+        close(this->s_socket);
+        return;
+    }
+
+    this->c_addr.Refresh();
+
+    this->s_success = true;
+}
---
+++ mysocket.h
@@ -0,0 +1,197 @@
+#ifndef FTP_MYSOCKET_H
+#define FTP_MYSOCKET_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <time.h>
+
+namespace MySocket {
+
+    /****** CLASS ADRESS ******/
+
+    //Contient les informations d'une adresse
+    class Address {
+
+    private:
+
+        //Structure d'adressage
+        struct sockaddr_in s_addr;
+
+        //Chaine de caractère contenant l'adresse IP textuelle
+        char s_ip[16];
+
+        //Numéro de port de l'adresse
+        int s_port;
+
+        //Détermine si la création de l'adresse a réussi
+        bool s_success;
+
+    public:
+
+        /**
+         * Crée une adresse locale avec un numéro de port aléatoire
+         */
+        Address();
+
+        /**
+         * Crée une adresse locale
+         * Paramètre:
+         * - numéro de port (int)
+         */
+        Address(int);
+
+        /**
+         * Crée une adresse
+         * Paramètres:
+         * - adresse (const char *)
+         * - numéro de port (int)
+         */
+        Address(const char *, int);
+
+        /**
+         * Crée une adresse à partir d'une structure d'adressage
+         * Paramètre:
+         * - structure d'adressage (const struct sockaddr_in *)
+        */
+        Address(const struct sockaddr_in *);
+
+        /**
+         * Détermine si la création de l'adresse a réussi
+        */
+        bool Success() const;
+
+        /**
+         * Retourne l'adresse IP
+        */
+        const char * GetIP() const;
+
+        /*
+         * Retourne le numéro de port
+        */
+        int GetPort() const;
+
+        /**
+         * Retourne un pointeur vers la structure d'adressage
+        */
+        const struct sockaddr_in * GetSockaddr() const;
+
+        /**
+         * Met à jour les attributs de l'adresse
+        */
+        void Refresh();
+
+    private:
+
+        //Retourne l'adresse ip formaté en fonction de l'adresse textuelle
+        static in_addr_t _getipbyname(struct hostent *);
+
+        //Remplie une chaine de caractère avec l'adresse ip correspondant
+        static void _addrtoip(char *, const struct sockaddr_in *);
+
+        //Retourne le numéro de port correspondant
+        static int _addrtoportn(const struct sockaddr_in *);
+
+    };
+
+
+    /****** CLASS SOCKET_TCP ******/
+
+    //Permet d'utiliser des sockets avec TCP
+    class Socket_TCP {
+
+    private:
+
+        //Adresse de la cible (ou locale si c'est une BR publique)
+        Address c_addr;
+
+        //Descripteur de la socket
+        int s_socket;
+
+        //Détermine si la création de la socket a réussi
+        bool s_success;
+
+    public:
+
+        /**
+         * Crée une socket TCP pour serveur avec numéro de port aléatoire
+        */
+        Socket_TCP();
+
+        /**
+         * Crée une socket TCP pour serveur
+         * Paramètre:
+         * - numéro de port (int)
+        */
+        Socket_TCP(int);
+
+        /**
+         * Crée une socket TCP pour client
+         * Paramètre:
+         * - adresse (const char *)
+         * - numéro de port (int)
+        */
+        Socket_TCP(const char *, int);
+
+        /**
+         * Détermine si la création de la socket a réussi
+        */
+        bool Success();
+
+        /**
+         * Lance l'écoute du socket
+         * Paramètre:
+         * - longeur de la file (int)
+         * Retourne vrai si réussi et faux sinon
+        */
+        bool Listen(int);
+
+        /**
+         * Accepte une demande de connexion
+         * Retourne la socket associée
+        */
+        Socket_TCP Accept();
+
+        /**
+         * Retourne le destinataire de la socket
+        */
+        const Address& GetDestination();
+
+        /**
+         * Lit dans la socket
+         * Paramètres:
+         * - buffer (void *)
+         * - taille du buffer (int)
+         * Retourne le nombre de caractères lus
+        */
+        int Read(void *, int);
+
+        /**
+         * Ecrit dans la socket
+         * Paramètres:
+         * - données (void *)
+         * - taille (int)
+         * Retourne le nombre de caractères écrits
+        */
+        int Write(void *, int);
+
+    private:
+
+        //Initialise la class avec la socket et la cible
+        Socket_TCP(int, struct sockaddr_in *);
+
+        //Initialise la socket
+        void _init_serv();
+
+    };
+
+}
+
+#endif
